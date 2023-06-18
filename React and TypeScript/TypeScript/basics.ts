4ad22d3ac6326d1ea3ff564548b797e3378f6b20
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-06-18 18:28:17 +0300

    added reference values types

--- React and TypeScript/TypeScript/basics.ts
+++ React and TypeScript/TypeScript/basics.ts
@@ -6,3 +6,24 @@ myName = "Adrian";
 
 let isFun: boolean;
 isFun = true;
+
+let programmingLanguages: string[];
+programmingLanguages = ["JavaScript", "TypeScript"];
+
+let person: {
+    name: string;
+    age: number;
+};
+person = {
+    name: "Adrian",
+    age: 29
+};
+
+let people: {
+    name: string;
+    age: number;
+}[];
+
+let inferenceString = "Inference string";
+
+let unionStringAndNumber: string | number = "This can bea string or a number";
